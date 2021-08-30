50d8e9ff6a2fd773e077066af0a07a4d0ef8a7c9
kim <zxcv9203@naver.com>
2021-08-30 14:06:52 +0900

    test

--- 변수와 상수/var.go
+++ 변수와 상수/var.go
@@ -1,1 +1,30 @@
-package 변수와_상수
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+var (
+	myInt int
+	myFloat float64
+	myBool bool
+	myString string
+)
+
+func main() {
+	var a int
+	var b float64 = 10.5
+
+	a = 10
+	b = 42.42
+
+	var c, d int
+	c, d = 24, 42
+	var e, f float64 = 0.42, 4.2
+	var g = 3
+	fmt.Printf("a = %d, b = %f\n", a, b)
+	fmt.Printf("c = %d, d = %d\n", c, d)
+	fmt.Printf("e = %f, f = %f\n", e, f)
+	fmt.Printf("g = %d, g의 type = %s", g, reflect.TypeOf(g))
+}
