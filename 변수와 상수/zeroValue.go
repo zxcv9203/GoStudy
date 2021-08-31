66376fca332fda638d39b2098fb6036c08848910
yongckim <zxcv9203@naver.com>
2021-08-31 21:22:18 +0900

    변수와 상수

---
+++ 변수와 상수/zeroValue.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println("int의 제로 값 : ",a)
+	fmt.Println("float64의 제로 값 : ",b)
+	fmt.Println("string의 제로 값 : ",c)
+	fmt.Println("bool의 제로 값 : ",d)
+}
